111d2ff164b9f7ed99c45db06ba44a58b64305fd
kritzona <virell.m@yandex.ru>
2021-03-30 12:03:33 +0300

    Настроил React Perfect Scrollbar

--- src/components/atoms/List/ListStyled.tsx
+++ src/components/atoms/List/ListStyled.tsx
@@ -9,6 +9,8 @@ const ListStyled = styled.div<IProps>`
     width: 100%;
     height: 100%;
 
+    padding-right: ${theme.sizes.stepSize * 1.5}px;
+
     & > *:not(:first-child) {
       margin-top: ${theme.sizes.stepSize * 2.5}px;
     }
